a418c3416f86ddbeeb9c39384f4c12d45bf6c502
Belvan Forrati <sempretemgentedeolho@gmail.com>
2026-08-02 02:09:31 +0000

    File changes

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -3,8 +3,8 @@ import { cn } from '@/components/ui/utils';
 
 export function PhoneFrame({ children, className }) {
   return (
-    <div className="w-full h-[100dvh] bg-black flex items-center justify-center">
-      <div className="relative h-full aspect-[9/20] bg-black p-[3px] rounded-[2.75rem] shadow-2xl mx-auto max-w-full">
+    <div className="w-full h-[100dvh] bg-black flex items-center justify-center overflow-hidden">
+      <div className="relative h-[100dvh] aspect-[9/20] bg-black p-[3px] rounded-[2.75rem] shadow-2xl">
         {/* Notch */}
         <div className="absolute top-0 left-1/2 -translate-x-1/2 w-32 h-6 bg-black rounded-b-2xl z-20 flex items-center justify-center gap-2">
           <div className="w-14 h-1.5 bg-slate-800 rounded-full"></div>
