cc2d7828a32bd8f46f8c007c5225305cfda7cf31
Belvan Forrati <sempretemgentedeolho@gmail.com>
2026-08-02 02:00:34 +0000

    File changes

--- src/components/PhoneFrame.jsx
+++ src/components/PhoneFrame.jsx
@@ -3,23 +3,20 @@ import { cn } from '@/components/ui/utils';
 
 export function PhoneFrame({ children, className }) {
   return (
-    <div className="w-full min-h-[100dvh] bg-black flex items-center justify-center p-0 sm:p-4">
-      <div className="relative w-full h-[100dvh] sm:w-auto sm:h-[calc(100dvh-2rem)] sm:aspect-[9/19.5] sm:rounded-[2.5rem] sm:shadow-2xl">
-        {/* Phone Border */}
-        <div className="relative bg-black h-full p-0 sm:p-3 sm:rounded-[2.5rem]">
-          {/* Notch */}
-          <div className="absolute top-0 left-1/2 -translate-x-1/2 w-40 h-7 bg-black rounded-b-3xl z-20 flex items-center justify-center gap-2">
-            <div className="w-16 h-1.5 bg-slate-800 rounded-full"></div>
-            <div className="w-3 h-3 bg-slate-900 rounded-full border border-slate-700"></div>
-          </div>
+    <div className="w-full h-[100dvh] bg-black flex items-center justify-center">
+      <div className="relative w-full h-full max-w-[480px] mx-auto bg-black">
+        {/* Notch */}
+        <div className="absolute top-0 left-1/2 -translate-x-1/2 w-40 h-7 bg-black rounded-b-3xl z-20 flex items-center justify-center gap-2">
+          <div className="w-16 h-1.5 bg-slate-800 rounded-full"></div>
+          <div className="w-3 h-3 bg-slate-900 rounded-full border border-slate-700"></div>
+        </div>
 
-          {/* Screen */}
-          <div className={cn(
-            "relative bg-white rounded-[2rem] overflow-hidden h-full",
-            className
-          )}>
-            {children}
-          </div>
+        {/* Screen */}
+        <div className={cn(
+          "relative bg-white overflow-hidden h-full w-full",
+          className
+        )}>
+          {children}
         </div>
       </div>
     </div>
